83d61023e36a24b47c6ab91d45c756f2abff134a
Will Williams <wwilliams@indeed.com>
2024-02-26 16:01:41 -0800

    Implements recursive solution

--- src/power.js
+++ src/power.js
@@ -5,7 +5,16 @@
  * @param {integer} exponent a non-negative integer
  */
 function power(base, exponent) {
- 
+  // Base case: if exponent is 0, return 1
+  if (exponent === 0) {
+    return 1;
+  }
+  // If exponent is negative, throw an Error
+  if (exponent < 0) {
+    throw new Error("exponent should be >= 0");
+  }
+  // Recursive case: compute power recursively
+  return base * power(base, exponent - 1);
 }
 
 module.exports = power;
