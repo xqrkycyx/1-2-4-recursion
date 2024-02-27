534fc71fe766aff5d3806c913ddee9b06ddb2bc8
Will Williams <wwilliams@indeed.com>
2024-02-26 16:36:14 -0800

    Implements recursive solution for Fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -2,6 +2,17 @@
  * Return the nth fibonacci number
  * @param {integer} n
  */
-function fibonacci(n) {}
+function fibonacci(n) {
+  // Base cases
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  // Recursive case
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 module.exports = fibonacci;
