1c8f8db326da840002983f0be251b1d657d141f3
Will Williams <wwilliams@indeed.com>
2024-02-26 16:39:02 -0800

    Implements recursive solution for String Reverse

--- src/reverse.js
+++ src/reverse.js
@@ -2,6 +2,13 @@
  * Returns the string with the characters in reverse order
  * @param {string} text the string to be reversed
  */
-function reverse(text) {}
+function reverse(text) {
+  // Base case: if the string has 0 or 1 characters, return the string itself
+  if (text.length <= 1) {
+    return text;
+  }
+  // Recursive case: return the last character followed by the reversed substring
+  return text.slice(-1) + reverse(text.slice(0, -1));
+}
 
 module.exports = reverse;
